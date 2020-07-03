752887a3143583362aada1f2096d98d4a061c3ae
root <pedro.042013@gmail.com>
2020-07-03 11:13:35 -0300

    dag_cdm

---
+++ dags/dag_cdm.py
@@ -0,0 +1,42 @@
+# Importando as bibliotecas que vamos usar nesse exemplo
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+# Definindo alguns argumentos básicos
+default_args = {
+    'owner': 'miguel_pedro',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 6, 29),
+    'retries': 1,
+    'email': ['inteligencia@crd.med.br'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+}
+# Nomeando a DAG e definindo quando ela vai ser executada (você pode usar argumentos em Crontab também caso queira que a DAG execute por exemplo todos os dias as 8 da manhã)
+with DAG(
+    'dag_cdm',
+    catchup=False,
+    schedule_interval= '0 0,12 * * *',
+    default_args=default_args
+) as dag:
+    # Definindo as tarefas que a DAG vai executar, nesse caso a execução de dois programas Python, chamando sua execução por comandos bash
+    t1 = BashOperator(
+        task_id='etl_atendimentos_cdm',
+        bash_command="""
+        cd $AIRFLOW_HOME/dags/etl_scripts/
+        python3 etl_atendimentos_cdm.py
+    """)
+    t2 = BashOperator(
+        task_id='etl_exames_cdm',
+        bash_command="""
+        cd $AIRFLOW_HOME/dags/etl_scripts/
+        python3 etl_exames_cdm.py
+    """)
+    t3 = BashOperator(
+        task_id='etl_pacientes_cdm',
+        bash_command="""
+        cd $AIRFLOW_HOME/dags/etl_scripts/
+        python3 etl_pacientes_cdm.py
+    """)
+# Definindo o padrão de execução, nesse caso executamos t1 e depois t2
+t1 >> t2 >> t3
